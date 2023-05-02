d7674d8c09284f245bf957cc365394b30f7cbbe1
deepak <patilrhdeepak@gmail.com>
2023-05-02 12:35:38 +0530

    First Time Pushing the code from local repo to global repo.

---
+++ src/test/java/Practice_Test1.java
@@ -0,0 +1,11 @@
+import org.testng.annotations.Test;
+
+public class Practice_Test1 {
+
+	
+	@Test
+	public void start()
+	{
+		System.out.println("start of test!");
+	}
+}
---
+++ src/test/java/Practice_Test2.java
@@ -0,0 +1,9 @@
+import org.testng.annotations.Test;
+
+public class Practice_Test2 {
+
+	@Test
+	public void test() {
+		System.out.println("i am");
+	}
+}
